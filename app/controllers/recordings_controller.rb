4970ee175f8200c8dd6124008431a756d7609f45
shap42103 <97652174+shap42103@users.noreply.github.com>
2022-09-06 20:52:33 +0900

    [Fix]APIタイプの変更およびActiveStorageを追加 (#39)
    * [Fix]APIタイプの変更およびActiveStorageを追加
    
    * [Fix]録音画面のフォームを隠し要素に修正

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -1,15 +1,26 @@
 class RecordingsController < ApplicationController
   def new
-    @themes = Theme.all
+    @themes = Theme.where.not(title: t('defaults.uploaded_voice'))
     @recording = Recording.new
     gon.api_key = ENV['API_KEY']
   end
 
   def create
     @recording = current_user.recordings.new(recording_params)
-    
-    theme_title = theme_params[:theme].fetch(:title)
+    if @recording.audio.attached?
+      if recording_params[:theme]
+        theme_title = theme_params[:theme].fetch(:title)
+      else
+        theme_title = t('defaults.uploaded_voice')
+      end
+    end
     @recording.set_theme(theme_title)
+
+    if @recording.parse_base64
+      @recording.voice = 'recorded'
+    else
+      @recording.voice = 'uploaded'
+    end
 
     if @recording.save
       redirect_to new_recording_text_analysis_path(@recording)
@@ -26,7 +37,7 @@ class RecordingsController < ApplicationController
   private
 
   def recording_params
-    params.require(:recording).permit(:voice, :text, :length)
+    params.require(:recording).permit(:voice, :text, :length, :audio)
   end
   
   def theme_params

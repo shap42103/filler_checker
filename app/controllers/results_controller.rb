a90d884cdd6b0db30f7d722f679c83fb318f23cf
keigo_nakamura <nakamura.keigo13@gmail.com>
2022-09-24 12:16:53 +0900

    [Add]ツイートボタンの実装

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -28,11 +28,40 @@ class ResultsController < ApplicationController
     @recording = Recording.find(params[:recording_id])
     @result = Result.find_by(recording_id: @recording.id)
     not_authenticated unless @recording.user_id == current_user.id
+    @tweet_url = tweet_url(@result)
   end
 
   private
 
   def result_params
     params.require(:result).permit(:recording_id, :filler_count, :most_frequent_filler)
+  end
+
+  def tweet_url(result)
+    params = URI.encode_www_form({
+      'url': request.base_url, 
+      'text': tweet_text(result), 
+      'hashtags': 'えーとチェッカー'
+    })
+    return "https://twitter.com/share?#{params}"
+  end
+
+  def tweet_text(result)
+    texts = []
+    texts[0] = "つなぎ言葉（フィラー）の回数は#{result.filler_interval_text}でした\n\n"
+    
+    if result.filler_interval.infinite? == 1
+      texts[0] = "つなぎ言葉（フィラー）はありませんでした！\n\n"
+      texts[1] = "#{current_user.name}は\n一切のよどみなく人にものごとを伝えられる\nスピーチマスターです！"
+    else
+     if result.filler_interval < 5
+      texts[1] = "#{result.most_frequent_filler_word}・・・、\n#{current_user.name}はフィラー癖が多いほうです。\n善処します"
+     else
+      texts[1] = "#{current_user.name}は\nたまに#{result.most_frequent_filler_word}・・・といってしまいますが、\n気になるほどではないです。"
+     end
+    end
+
+    texts[2] = "\n\nえーと…を言わずに話せるか試そう！"
+    return texts.sum
   end
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -2,4 +2,21 @@ class Result < ApplicationRecord
   belongs_to :recording
 
   validates :filler_count, presence: true
+
+  def filler_interval
+    (self.recording[:length] / self.filler_count.to_f).round(1)
+  end
+
+  def filler_interval_text
+    if self.filler_count.to_i == 0
+      'フィラーなし'
+    else
+      "#{self.filler_interval}秒に1回"
+      # t('defaults.per_count')
+    end
+  end
+
+  def most_frequent_filler_word
+    self.most_frequent_filler.gsub('%', '')
+  end
 end

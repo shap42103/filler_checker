1c6e25d137530393b78565c926b1e022d7b14e60
keigo_nakamura <nakamura.keigo13@gmail.com>
2022-09-25 21:55:39 +0900

    [Fix]ランキングページをログインなしで閲覧可能に修正

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,4 +1,6 @@
 class RankingsController < ApplicationController
+  skip_before_action :require_login, only: %i[index]
+
   def index
     @themes = Theme.all
     @results = Result.joins(:recording).limit(5)

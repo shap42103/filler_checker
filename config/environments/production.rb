91d5269a6b921e693685e263641dc3ccee411b61
keigo_nakamura <nakamura.keigo13@gmail.com>
2022-10-08 08:26:48 +0900

    [Fix]パスワードリセットを本番環境用に修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,20 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "filler_checker_production"
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = Settings.default_url_options.to_h
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    domain: 'gmail.com',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
